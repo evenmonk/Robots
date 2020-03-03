efdc2bfc0666c54501f0e421ca518b9d7546d6c6
erdelevsky <zh.vibe18@yandex.ru>
2020-03-03 23:59:31 +0500

    Update MainApplicationFrame.java

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -49,6 +49,8 @@ public class MainApplicationFrame extends JFrame
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     
+    
+    
     protected LogWindow createLogWindow()
     {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
@@ -112,6 +114,20 @@ public class MainApplicationFrame extends JFrame
             });
             lookAndFeelMenu.add(systemLookAndFeel);
         }
+        
+        JMenu exitMenu = new JMenu("Меню выхода");
+        JMenuItem exitButton = new JMenuItem("Закрыть приложение");
+         UIManager.put("OptionPane.yesButtonText" , "Да" );
+         UIManager.put("OptionPane.noButtonText" , "Нет" );
+
+         exitButton.addActionListener((event) -> {
+         int reply = JOptionPane.showConfirmDialog(null,
+         "Вы действительно хотите выйти?", "Выйти", JOptionPane.YES_NO_OPTION);
+         if (reply == JOptionPane.YES_OPTION)
+         System.exit(0);
+         });
+         exitMenu.add(exitButton);
+         menuBar.add(exitMenu);
 
         {
             JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
