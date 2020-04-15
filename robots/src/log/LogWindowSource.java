84af1c4989337d69131253100387cef93469d677
evenmonk <zh.vibe18@yandex.ru>
2020-04-15 21:01:49 +0500

    resource leak has been fixed

--- robots/src/log/LogWindowSource.java
+++ robots/src/log/LogWindowSource.java
@@ -1,85 +1,68 @@
 package log;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
-/**
- * Что починить:
- * 1. Этот класс порождает утечку ресурсов (связанные слушатели оказываются
- * удерживаемыми в памяти)
- * 2. Этот класс хранит активные сообщения лога, но в такой реализации он 
- * их лишь накапливает. Надо же, чтобы количество сообщений в логе было ограничено 
- * величиной m_iQueueLength (т.е. реально нужна очередь сообщений 
- * ограниченного размера) 
- */
-public class LogWindowSource
-{
-    private ArrayList<LogEntry> m_messages;
+public class LogWindowSource {
+
+    private int m_iQueueLength;
+    private ArrayDeque<LogEntry> m_messages;
     private final ArrayList<LogChangeListener> m_listeners;
-    private volatile LogChangeListener[] m_activeListeners;
 
     public LogWindowSource(int iQueueLength) {
-        m_messages = new ArrayList<LogEntry>(iQueueLength);
-        m_listeners = new ArrayList<LogChangeListener>();
+        m_iQueueLength = iQueueLength;
+        m_messages = new ArrayDeque<>(iQueueLength);
+        m_listeners = new ArrayList<>();
     }
-    
-    public void registerListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
+
+    public void registerListener(LogChangeListener listener) {
+        synchronized (m_listeners) {
             m_listeners.add(listener);
-            m_activeListeners = null;
         }
     }
-    
-    public void unregisterListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
+
+    public void unregisterListener(LogChangeListener listener) {
+        synchronized (m_listeners) {
             m_listeners.remove(listener);
-            m_activeListeners = null;
         }
     }
-    
-    public void append(LogLevel logLevel, String strMessage)
-    {
-        LogEntry entry = new LogEntry(logLevel, strMessage);
-        m_messages.add(entry);
-        LogChangeListener [] activeListeners = m_activeListeners;
-        if (activeListeners == null)
-        {
-            synchronized (m_listeners)
-            {
-                if (m_activeListeners == null)
-                {
-                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
-                    m_activeListeners = activeListeners;
-                }
-            }
-        }
-        for (LogChangeListener listener : activeListeners)
-        {
-            listener.onLogChanged();
-        }
+
+    public void append(LogLevel logLevel, String strMessage) {
+        addEntry(logLevel, strMessage);
+        notifyListeners();
     }
-    
-    public int size()
-    {
+
+    public int size() {
         return m_messages.size();
     }
 
-    public Iterable<LogEntry> range(int startFrom, int count)
-    {
-        if (startFrom < 0 || startFrom >= m_messages.size())
-        {
+    public Iterable<LogEntry> range(int startFrom, int count) {
+        if (startFrom < 0) {
             return Collections.emptyList();
         }
-        int indexTo = Math.min(startFrom + count, m_messages.size());
-        return m_messages.subList(startFrom, indexTo);
+        return m_messages.stream()
+                .skip(startFrom)
+                .limit(count)
+                .collect(Collectors.toList());
     }
 
-    public Iterable<LogEntry> all()
-    {
-        return m_messages;
+    public Iterable<LogEntry> all() {
+        return m_messages.clone();
+    }
+
+    private void addEntry(LogLevel logLevel, String strMessage) {
+        if (m_messages.size() == m_iQueueLength)
+            m_messages.poll();
+        m_messages.add(new LogEntry(logLevel, strMessage));
+    }
+
+    private void notifyListeners() {
+        synchronized (m_listeners) {
+            for (var listener : m_listeners) {
+                listener.onLogChanged();
+            }
+        }
     }
 }
